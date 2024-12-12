ff7992932fbb30c3557816916878ff035bf8ff98
LarkXa <larkxa@gmail.com>
2024-12-12 16:51:04 +0800

    自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+//Element Plus 按需要导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+  plugins: [ AutoImport({
+    resolvers: [ElementPlusResolver()],
+  }),
+  Components({
+    resolvers: [ElementPlusResolver()],
+  }),
     vue(),
     vueDevTools(),
   ],
