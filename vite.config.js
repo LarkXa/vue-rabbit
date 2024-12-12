8f40400db4594da5d04b89e053312fb7493c8e40
LarkXa <larkxa@gmail.com>
2024-12-12 17:09:34 +0800

    完成了主题色的覆盖

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,9 @@ export default defineConfig({
     resolvers: [ElementPlusResolver()],
   }),
   Components({
-    resolvers: [ElementPlusResolver()],
+    resolvers: [
+      //1.配置 Element Plus 采用sass配色系统
+      ElementPlusResolver({importStyle: 'sass'})],
   }),
     vue(),
     vueDevTools(),
@@ -24,5 +26,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
+  },css: {
+    preprocessorOptions: {
+      scss: {
+        //2.覆盖，自动导入定制化样式文件进行样式覆盖
+        additionalData: `@use "@/styles/element/index.scss" as *;`
+      }
+    }
+  }
 })
